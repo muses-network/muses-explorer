841edbc24b18ea8f8f419ea31f3934f670f2bdd7
Jassen <jassenlyou@foxmail.com>
2022-05-06 18:20:50 +0800

    feat: remove cosmos group box

--- src/@core/layouts/utils.js
+++ src/@core/layouts/utils.js
@@ -7,9 +7,9 @@ import { computed } from '@vue/composition-api'
  * @param {Object} item nav menu item
  */
 export const resolveVerticalNavMenuItemComponent = item => {
-  console.log('item:', item)
   if (item.header) return 'vertical-nav-menu-header'
   if (item.children) return 'vertical-nav-menu-group'
+  if (item.logo) return null
   return 'vertical-nav-menu-link'
 }
 

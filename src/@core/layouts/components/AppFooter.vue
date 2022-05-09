388b9c7eead49fc49deb0140c8ee770f7bf843ad
wanbowen <wbw157@gmail.com>
2022-05-09 16:21:09 +0800

    feat: modify app footer,del coffee component

--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,45 +1,46 @@
 <template>
-  <p class="clearfix mb-0">
-    <span class="float-md-left d-none d-md-block d-md-inline-block mt-25">
-      Powered By
-      <b-link
-        class="ml-25 font-weight-bolder"
-        href="#"
-        target="_blank"
-      >Muses Explorer</b-link>
-    </span>
+  <p></p>
+<!--  <p class="clearfix mb-0">-->
+<!--    <span class="float-md-left d-none d-md-block d-md-inline-block mt-25">-->
+<!--      Powered By-->
+<!--      <b-link-->
+<!--        class="ml-25 font-weight-bolder"-->
+<!--        href="#"-->
+<!--        target="_blank"-->
+<!--      >Muses Explorer</b-link>-->
+<!--    </span>-->
 
-    <router-link
-      v-ripple.400="'rgba(113, 102, 240, 0.15)'"
-      class="float-md-right"
-      to="/coffee"
-    >Buy me a cup of coffee.
-      <span
-        v-ripple.400="'rgba(113, 102, 240, 0.15)'"
-        variant="outline-primary"
-      >
-        <feather-icon
-          icon="HeartIcon"
-          size="21"
-          class="text-danger stroke-current"
-        />
-      </span>
-    </router-link>
-  </p>
+<!--    <router-link-->
+<!--      v-ripple.400="'rgba(113, 102, 240, 0.15)'"-->
+<!--      class="float-md-right"-->
+<!--      to="/coffee"-->
+<!--    >Buy me a cup of coffee.-->
+<!--      <span-->
+<!--        v-ripple.400="'rgba(113, 102, 240, 0.15)'"-->
+<!--        variant="outline-primary"-->
+<!--      >-->
+<!--        <feather-icon-->
+<!--          icon="HeartIcon"-->
+<!--          size="21"-->
+<!--          class="text-danger stroke-current"-->
+<!--        />-->
+<!--      </span>-->
+<!--    </router-link>-->
+<!--  </p>-->
 </template>
 
 <script>
-import { BLink, VBPopover } from 'bootstrap-vue'
-import Ripple from 'vue-ripple-directive'
+// import { BLink, VBPopover } from 'bootstrap-vue'
+// import Ripple from 'vue-ripple-directive'
 
 export default {
   name: 'AppFooter',
   components: {
-    BLink,
+    // BLink,
   },
   directives: {
-    'b-popover': VBPopover,
-    Ripple,
+    // 'b-popover': VBPopover,
+    // Ripple,
   },
 }
 </script>
